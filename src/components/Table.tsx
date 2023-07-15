d7fb4dd9932edfbfd7dc6d50e0dfdb002769ba09
Diego França <diego.franca92@gmail.com>
2023-07-14 21:39:32 -0300

    feat: include Material Tailwind and refactor Navbar and Table components

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,44 +1,259 @@
-type TableProps = {
-  title?: string
-  size?: number
-}
+import {
+  MagnifyingGlassIcon,
+  ChevronUpDownIcon
+} from '@heroicons/react/24/outline'
+import {
+  PencilIcon,
+  UserPlusIcon,
+  ArrowRightIcon,
+  ArrowLeftIcon
+} from '@heroicons/react/24/solid'
+import {
+  Card,
+  CardHeader,
+  Input,
+  Typography,
+  Button,
+  CardBody,
+  Chip,
+  CardFooter,
+  Tabs,
+  TabsHeader,
+  Tab,
+  Avatar,
+  IconButton,
+  Tooltip
+} from '@material-tailwind/react'
+import React from 'react'
 
-export default function Table({ title, size }: TableProps) {
+const TABS = [
+  {
+    label: 'All',
+    value: 'all'
+  },
+  {
+    label: 'Monitored',
+    value: 'monitored'
+  },
+  {
+    label: 'Unmonitored',
+    value: 'unmonitored'
+  }
+]
+
+const TABLE_HEAD = ['Member', 'Function', 'Status', 'Employed', '']
+
+const TABLE_ROWS = [
+  {
+    img: 'https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/team-3.jpg',
+    name: 'John Michael',
+    email: 'john@creative-tim.com',
+    job: 'Manager',
+    org: 'Organization',
+    online: true,
+    date: '23/04/18'
+  },
+  {
+    img: 'https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/team-2.jpg',
+    name: 'Alexa Liras',
+    email: 'alexa@creative-tim.com',
+    job: 'Programator',
+    org: 'Developer',
+    online: false,
+    date: '23/04/18'
+  },
+  {
+    img: 'https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/team-1.jpg',
+    name: 'Laurent Perrier',
+    email: 'laurent@creative-tim.com',
+    job: 'Executive',
+    org: 'Projects',
+    online: false,
+    date: '19/09/17'
+  },
+  {
+    img: 'https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/team-4.jpg',
+    name: 'Michael Levi',
+    email: 'michael@creative-tim.com',
+    job: 'Programator',
+    org: 'Developer',
+    online: true,
+    date: '24/12/08'
+  },
+  {
+    img: 'https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/team-5.jpg',
+    name: 'Richard Gran',
+    email: 'richard@creative-tim.com',
+    job: 'Manager',
+    org: 'Executive',
+    online: false,
+    date: '04/10/21'
+  }
+]
+
+export default function Table() {
+  const [active, setActive] = React.useState(1)
+
+  const getItemProps = (index:number) =>
+    ({
+      variant: active === index ? 'filled' : 'text',
+      color: active === index ? 'blue' : 'blue-gray',
+      onClick: () => setActive(index),
+      className: 'rounded-full'
+    } as any)
+
+  const next = () => {
+    if (active === 5) return
+
+    setActive(active + 1)
+  }
+
+  const prev = () => {
+    if (active === 1) return
+
+    setActive(active - 1)
+  }
   return (
-    <div className='w-ful rounded-md p-4'>
-      <header className='bg-slate-400 text-white text-xl'>{title}</header>
-      <table
-        width={size}
-        className='w-full border-2 rounded-sm'>
-        <thead className='p-8 border-b-2'>
-          <tr>
-            <th>
-              <input type='checkbox' />
-            </th>
-            <th>Name</th>
-            <th>Age</th>
-            <th>City</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>
-              <input type='checkbox' />
-            </td>
-            <td>John</td>
-            <td>22</td>
-            <td>London</td>
-          </tr>
-          <tr>
-            <td>
-              <input type='checkbox' />
-            </td>
-            <td>Jane</td>
-            <td>23</td>
-            <td>Paris</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+    <Card className='h-full w-full'>
+      <CardHeader floated={false} shadow={false} className='rounded-none'>
+        <div className='flex flex-col items-center justify-between gap-4 md:flex-row'>
+          <div className='w-full md:w-72'>
+            <Input
+              label='Search'
+              icon={<MagnifyingGlassIcon className='h-5 w-5' />}
+            />
+          </div>
+          <Button className='flex items-center gap-3' color='blue' size='sm'>
+              <UserPlusIcon strokeWidth={2} className='h-4 w-4' /> Add member
+            </Button>
+        </div>
+      </CardHeader>
+      <CardBody className='overflow-scroll px-0'>
+        <table className='mt-4 w-full min-w-max table-auto text-left'>
+          <thead>
+            <tr>
+              {TABLE_HEAD.map((head, index) => (
+                <th
+                  key={head}
+                  className='cursor-pointer border-y border-blue-gray-100 bg-blue-gray-50/50 p-4 transition-colors hover:bg-blue-gray-50'>
+                  <Typography
+                    variant='small'
+                    color='blue-gray'
+                    className='flex items-center justify-between gap-2 font-normal leading-none opacity-70'>
+                    {head}{' '}
+                    {index !== TABLE_HEAD.length - 1 && (
+                      <ChevronUpDownIcon strokeWidth={2} className='h-4 w-4' />
+                    )}
+                  </Typography>
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {TABLE_ROWS.map(
+              ({ img, name, email, job, org, online, date }, index) => {
+                const isLast = index === TABLE_ROWS.length - 1
+                const classes = isLast
+                  ? 'p-4'
+                  : 'p-4 border-b border-blue-gray-50'
+
+                return (
+                  <tr key={name}>
+                    <td className={classes}>
+                      <div className='flex items-center gap-3'>
+                        <Avatar src={img} alt={name} size='sm' />
+                        <div className='flex flex-col'>
+                          <Typography
+                            variant='small'
+                            color='blue-gray'
+                            className='font-normal'>
+                            {name}
+                          </Typography>
+                          <Typography
+                            variant='small'
+                            color='blue-gray'
+                            className='font-normal opacity-70'>
+                            {email}
+                          </Typography>
+                        </div>
+                      </div>
+                    </td>
+                    <td className={classes}>
+                      <div className='flex flex-col'>
+                        <Typography
+                          variant='small'
+                          color='blue-gray'
+                          className='font-normal'>
+                          {job}
+                        </Typography>
+                        <Typography
+                          variant='small'
+                          color='blue-gray'
+                          className='font-normal opacity-70'>
+                          {org}
+                        </Typography>
+                      </div>
+                    </td>
+                    <td className={classes}>
+                      <div className='w-max'>
+                        <Chip
+                          variant='ghost'
+                          size='sm'
+                          value={online ? 'online' : 'offline'}
+                          color={online ? 'green' : 'blue-gray'}
+                        />
+                      </div>
+                    </td>
+                    <td className={classes}>
+                      <Typography
+                        variant='small'
+                        color='blue-gray'
+                        className='font-normal'>
+                        {date}
+                      </Typography>
+                    </td>
+                    <td className={classes}>
+                      <Tooltip content='Edit User'>
+                        <IconButton variant='text' color='blue-gray'>
+                          <PencilIcon className='h-4 w-4' />
+                        </IconButton>
+                      </Tooltip>
+                    </td>
+                  </tr>
+                )
+              }
+            )}
+          </tbody>
+        </table>
+      </CardBody>
+      <CardFooter className='flex items-center justify-center border-t border-blue-gray-50 p-4'>
+        <div className='flex items-center gap-4'>
+          <Button
+            variant='text'
+            color='blue-gray'
+            className='flex items-center gap-2 rounded-full'
+            onClick={prev}
+            disabled={active === 1}>
+            <ArrowLeftIcon strokeWidth={2} className='h-4 w-4' /> Previous
+          </Button>
+          <div className='flex items-center gap-2'>
+            <IconButton {...getItemProps(1)}>1</IconButton>
+            <IconButton {...getItemProps(2)}>2</IconButton>
+            <IconButton {...getItemProps(3)}>3</IconButton>
+            <IconButton {...getItemProps(4)}>4</IconButton>
+            <IconButton {...getItemProps(5)}>5</IconButton>
+          </div>
+          <Button
+            variant='text'
+            color='blue-gray'
+            className='flex items-center gap-2 rounded-full'
+            onClick={next}
+            disabled={active === 5}>
+            Next
+            <ArrowRightIcon strokeWidth={2} className='h-4 w-4' />
+          </Button>
+        </div>
+      </CardFooter>
+    </Card>
   )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,26 +1,19 @@
-import imgPerfil from '../assets/perfil.jpg'
 import Table from '../components/Table'
 import CardDash from '../components/CardDash'
+import NavBar from '../components/NavBar'
 
 export default function Home() {
-  
-  
   return (
     <>
-      <div className="min-h-full">
-        <header className="bg-white shadow">
-          <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 flex justify-between">
-            <h1 className="text-3xl font-bold tracking-tight inline text-gray-900">Dashboard</h1>
-            <img width={60} height={60} className='rounded-full' src={imgPerfil} alt=''/>
-          </div>
-        </header>
+      <div className='min-h-full'>
+        <NavBar />
         <main>
-          <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8 flex gap-4">
+          <div className='mx-auto max-w-7xl py-6 sm:px-6 lg:px-8 flex gap-4'>
             <CardDash title='Empresas' value={453} />
             <CardDash title='Funcionarios' value={453} />
             <CardDash title='Ganho' value={453} />
           </div>
-          <Table title='Empresas' />
+          <Table />
         </main>
       </div>
     </>
